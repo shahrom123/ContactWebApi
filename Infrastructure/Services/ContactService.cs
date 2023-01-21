a2c28e6b708d5921ba909de55709218636ce83fc
shahrom123 <118911986+shahrom123@users.noreply.github.com>
2023-01-21 16:28:55 +0500

    first commit

---
+++ Infrastructure/Services/ContactService.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Npgsql;
+using Domain.Entities;
+
+namespace Infrastructure.Services
+{
+    public class ContactService
+    {
+        private string _connectionString = "Server=127.0.0.1; Port=5432;Database=ContactDb ; User Id=postgres; Password=233223;";
+
+        public List<Contact> GetContact()
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM Contacts";
+                var result = connection.Query<Contact>(sql).ToList();
+                return result; 
+            }
+        }
+        public List<Contact> GetContactByName(string name)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                string sql = $"SELECT c.Id, c.Name, c.Address, c.Telephone FROM Contacts c where Name = '{name}';";
+                var result = connection.Query<Contact>(sql).ToList();
+                return result;
+            }
+        }
+        public bool AddContact(Contact contact)
+        {
+
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                var sql = $" insert into Contacts (id, Name, Address, Telephone)" +
+                    $" values ({contact.Id}, '{contact.Name}', '{contact.Address}',{contact.Telephone})";
+                var added = connection.Execute(sql);
+                if (added > 0) return true;
+                else return false;
+            }
+        }
+
+        public bool UpdateContact(Contact contact)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                var sql = $" update Contacts set Name = '{contact.Name}'," +
+                    $" Address = '{contact.Address}', Telephone = {contact.Telephone}" +
+                    $" where id = {contact.Id}";
+                var updated = connection.Execute(sql);
+                if (updated > 0) return true;
+                else return false;
+            }
+        }
+
+        public bool DeleteContact(int id)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                var sql = $" delete from Contacts  where id = {id}";
+                var deleted = connection.Execute(sql);
+                if (deleted > 0) return true;
+                else return false;
+            }
+        }
+    } 
+}
---
+++ WebApi/Controllers/ContactController.cs
@@ -0,0 +1,44 @@
+﻿
+using Domain.Entities;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController
+    {
+        private  ContactService _contactService;
+        public ContactController() 
+        {
+            _contactService =  new ContactService(); 
+        }
+        [HttpGet ("Get")]
+        public List<Contact> Get()
+        {
+            return _contactService.GetContact(); 
+        }
+        [HttpGet("GetByname")] 
+        public List<Contact> GetContactByName(string name)
+        {
+            return _contactService.GetContactByName(name);
+        }
+        [HttpPost("Add")] 
+        public bool Add(Contact contact)
+        {
+            return _contactService.AddContact(contact);
+        }
+        [HttpPut("Update")]
+        public bool Update(Contact contact)
+        {
+            return _contactService.UpdateContact(contact);
+        }
+
+        [HttpDelete("Delete")]  
+        public void Delete(int id)
+        {
+            _contactService.DeleteContact(id);
+        }
+    }
+}
